4322d3bff4d0cce6a8b05d2fa94dd28a99af0c62
jvalverde96 <javier96.vs@gmail.com>
2025-01-07 11:19:11 -0600

    fix: add 'ts' in jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  moduleFileExtensions: ['ts'],
+  moduleFileExtensions: ['ts', 'js'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
 };
